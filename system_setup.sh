e1dcf59c77c5778cc3db3b2bfa847646ca838c7c
adityaverm-a <aditya.se.403@gmail.com>
2024-02-16 16:48:58 +0530

    composer chnages

--- system_setup.sh
+++ system_setup.sh
@@ -12,17 +12,18 @@ fi
 
 # Updating package repositories
 echo "Running: apt update"
-sudo apt update
+sudo apt update -y
 
 # Installing Apache web server
 echo "Installing Apache"
-sudo apt-get install apache2
+sudo apt-get install apache2 -y
 
 # Installing PHP
 echo "Installing PHP"
-sudo apt-get purge 'php*'                   # Removing existing PHP packages
-sudo add-apt-repository ppa:ondrej/php      # Adding PHP repository
-sudo apt update
+sudo apt-get purge 'php*' -y                   # Removing existing PHP packages
+sudo apt install php -y
+#sudo add-apt-repository ppa:ondrej/php -y      # Adding PHP repository
+sudo apt update -y
 
 # Selecting PHP versions to install
 echo "Enter PHP versions to install (separated by spaces): "
@@ -38,16 +39,19 @@ echo "Installation of PHP versions ${versions[@]} completed!"
 
 # Installing Redis
 echo "Installing Redis"
-sudo apt-get install build-essential tcl8.6    # Installing prerequisites for Redis
-sudo apt install redis-server                    # Installing Redis server
-sudo nano /etc/redis/redis.conf                 # Opening Redis configuration file for editing
+sudo apt-get install build-essential tcl8.6 -y   # Installing prerequisites for Redis
+sudo apt install redis-server -y                    # Installing Redis server
+# sudo nano /etc/redis/redis.conf                 # Opening Redis configuration file for editing
 sudo sed -i 's/^supervised.*/supervised systemd/' /etc/redis/redis.conf  # Setting supervised directive to systemd
 
 # Installing Composer
 echo "Installing Composer"
-sudo apt-get install curl                        # Installing curl
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer  # Downloading and installing Composer
+sudo apt install php-cli unzip -y
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+#curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer  # Downloading and installing Composer
 
 # Installing Git
 echo "Installing Git"
-sudo apt install git                             # Installing Git
+sudo apt install git -y                             # Installing Git
